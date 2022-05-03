9a3a308ae7f5d5a6f0c977d231118349052ab08c
castorfou <castorfou@users.noreply.github.com>
2022-05-03 07:26:43 +0000

    deploy: 61941b129977c8e6c4917857a9c4a1cbbdd6d503

--- files/setup_wsl_06_git_credential_manager.sh
+++ files/setup_wsl_06_git_credential_manager.sh
@@ -4,6 +4,7 @@ tmp_dir=$(mktemp -d )
 wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb -P $tmp_dir
 sudo dpkg -i $tmp_dir/gcmcore-linux_amd64.2.0.696.deb
 git-credential-manager-core configure
+git config --global credential.credentialStore gpg
 
 if [ -e "/.cfg" ]; then
 		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
