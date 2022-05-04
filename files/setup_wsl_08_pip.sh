a7cc648508c75120e83fee99a949fd2adba28301
castorfou <castorfou@users.noreply.github.com>
2022-05-04 06:42:49 +0000

    deploy: 055c6bbe9751dbb9030a871ad4d0c279c1f16157

---
+++ files/setup_wsl_08_pip.sh
@@ -0,0 +1,23 @@
+echo "configure pip v1"
+
+mkdir -p ~/.config/pip
+tee ~/.config/pip/pip.conf << EOF
+[global]
+timeout = 1000
+index-url = https://pypi.org/simple/
+​
+trusted-host = download.pytorch.org
+               pypi.python.org
+               files.pythonhosted.org
+               pypi.org
+               artifactory.michelin.com
+​
+extra-index-url= https://artifactory.michelin.com/api/pypi/pypi/simple
+EOF
+
+if [ -e "/.cfg" ]; then
+		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
+		$config add ~/.config/pip/pip.conf
+		$config commit -m'config artifactory'
+		$config push		
+fi
