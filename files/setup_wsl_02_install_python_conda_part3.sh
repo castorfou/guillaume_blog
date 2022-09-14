ba5fa20dea00749cba28150952b91082e8d03eaf
guillaume <guillaume.ramelet@michelin.com>
2022-09-14 09:33:44 +0200

    2022-09-14_09:33

--- files/setup_wsl_02_install_python_conda_part3.sh
+++ files/setup_wsl_02_install_python_conda_part3.sh
@@ -3,14 +3,18 @@ echo "configure SSL cert v1"
 export SSL_CERT=`python -c 'import certifi;print(certifi.where())'`
 echo $SSL_CERT
 
-tee -a ~/.bashrc2 << EOF
-export REQUESTS_CA_BUNDLE=$SSL_CERT
+tee -a ~/.bashrc << EOF
+export SSL_CERT=$SSL_CERT
 EOF
 
-#if [ -e "/.cfg" ]; then
-#		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
-#		$config add ~/.bashrc
-#		$config commit -m'export certificates for commandline'
-#		$config push		
-#fi
+export TMPDIR=`mktemp -d`
+git clone git@gitlab.michelin.com:DEV/bib-certificates.git $TMPDIR
+cd $TMPDIR
+cat *trust-ca.pem >> $SSL_CERT
 
+if [ -e "/.cfg" ]; then
+		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
+		$config add ~/.bashrc
+		$config commit -m'export certificates for commandline'
+		$config push		
+fi
