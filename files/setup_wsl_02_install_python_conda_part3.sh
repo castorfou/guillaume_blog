e873f5cc96a1f341f02899302c6a054624911905
castorfou <castorfou@users.noreply.github.com>
2022-09-14 07:41:26 +0000

    deploy: 45d7865537705feb18e471bdaab3c3e738b393ac

--- files/setup_wsl_02_install_python_conda_part3.sh
+++ files/setup_wsl_02_install_python_conda_part3.sh
@@ -1,16 +1,15 @@
 echo "configure SSL cert v1"
 
-export SSL_CERT=`python -c 'import certifi;print(certifi.where())'`
-echo $SSL_CERT
+export SSL_CERT_FILE=`python -c 'import certifi;print(certifi.where())'`
 
 tee -a ~/.bashrc << EOF
-export SSL_CERT=$SSL_CERT
+export SSL_CERT_FILE=$SSL_CERT_FILE
 EOF
 
 export TMPDIR=`mktemp -d`
 git clone git@gitlab.michelin.com:DEV/bib-certificates.git $TMPDIR
 cd $TMPDIR
-cat *trust-ca.pem >> $SSL_CERT
+cat *trust-ca.pem >> $SSL_CERT_FILE
 
 if [ -e "/.cfg" ]; then
 		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
